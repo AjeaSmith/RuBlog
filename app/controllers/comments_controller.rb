6d77338e6b4c01495c718b087759a90ba7a69a8f
Ajea Smith <ajeasmith@Ajeas-MBP.attlocal.net>
2019-05-08 04:18:25 -0400

    added comment relationships

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
+  def create
+    # find the post by post_id
+    @post = Post.find(params[:post_id])
+    # create a comment on related post
+    @comment = @post.comments.create(params[:comment]).permit(:name, :acomment)
+    redirect_to post_path(@post)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_010428) do
+ActiveRecord::Schema.define(version: 2019_05_08_072359) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "acomment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
