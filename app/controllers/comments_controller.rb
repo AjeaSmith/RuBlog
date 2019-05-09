f98864549be93d6eb99f240e8ac3107c3f1b1e02
Ajea Smith <ajeasmith@Ajeas-MBP.attlocal.net>
2019-05-09 11:38:15 -0400

    finished comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,20 @@ class CommentsController < ApplicationController
     # find the post by post_id
     @post = Post.find(params[:post_id])
     # create a comment on related post
-    @comment = @post.comments.create(params[:comment]).permit(:name, :acomment)
+    @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :acomment)
   end
 end
